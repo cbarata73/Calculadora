b4525021dfc033d90239f80dabdf97e5d655c1e4
Carlos Barata <cbarata73@gmail.com>
2025-01-23 11:52:43 +0000

    Alteração da calculadora

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
     result = float("nan")
     if operador == '+':
         result = num1 + num2
+    elif operador == '-':
+        result = num1 - num2
+    elif operador == '*':
+        result = num1 * num2
+    elif operador == '/':
+        if num2 == 0:
+            return float("nan")
+        result = num1 / num2
     elif operador == '%':
         result = num1 % num2
     elif operador == '^':
